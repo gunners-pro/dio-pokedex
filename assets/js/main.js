0b8ffaa7ac9d38dc6250b45f726b8cd5037130f8
Fabricyo Barreto <bass.barreto@gmail.com>
2022-10-20 21:17:10 -0300

    refactor: change the functions that consume the api.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,25 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map(typeSlot => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon">
-      <span class="number">#001</span>
-      <span class="name">${pokemon.name}</span>
-      <div class="detail">
-        <ol class="types">
-          <li class="type">grass</li>
-          <li class="type">poison</li>
-        </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-          alt="${pokemon.name}">
-      </div>
-    </li>
+  <li class="pokemon">
+  <span class="number">#${pokemon.order}</span>
+  <span class="name">${pokemon.name}</span>
+  <div class="detail">
+  <ol class="types">
+  ${convertPokemonTypesToLi(pokemon.types).join('')}
+  </ol>
+  <img src="${pokemon.sprites.other.dream_world.front_default}"
+  alt="${pokemon.name}">
+  </div>
+  </li>
   `
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-  .then(response => response.json())
-  .then(jsonBody => jsonBody.results)
-  .then(pokemons => {
-    for (let i = 0; i < pokemons.length; i++) {
-      const pokemon = pokemons[i];
-      pokemonList.innerHTML += convertPokemonToLi(pokemon)
-    }
-  })
-  .catch(error => console.log(error))
+pokeApi.getPokemon().then(pokemons => {
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
